21850fdef138805b3d845dbec38d049bc898f0d6
Usuario <Usuario@CPD01-M01>
2024-04-11 15:57:44 -0300

    Adicionar arquivos de projeto.

---
+++ Inventory/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = { };
+
+            do
+            {
+
+            } while (true);
+
+
+
+           
+        }
+    }
+}
